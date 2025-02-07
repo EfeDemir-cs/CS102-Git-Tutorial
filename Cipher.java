bdc6c81b2ed377ec242bee1b2d998a824c5a3e3f
Your Name <efedemir1273@hotmail.com>
2025-02-07 10:58:25 +0300

    Small problems in the encripting and decripting parts are solved.

--- Cipher.java
+++ Cipher.java
@@ -7,27 +7,27 @@ public class Cipher
     public static final String ORIGINAL_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public static final String CIPHER_ALPHABET = "dfxyhrklvwuasgimnojpqetbcz";
 
-    public String encrypt(String inputString) {
+    public String encrypt(String inputString){
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
         // for all chars in the input string
-        for (int i = 0; i < inputString.length(); i++)   
-        {
-
+        for (int i = 0; i < inputString.length(); i++){
+            outputString += replaceChar(inputString.charAt(i), true);
         }
 
         return outputString;
     }
 
-    public String decrypt(String inputString) {
+    public String decrypt(String inputString){
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
-        replaceChar('a',true);
-        
+        for (int i = 0; i < inputString.length(); i++){
+            outputString += replaceChar(inputString.charAt(i), false);
+        }
         return outputString;
     }
 
@@ -36,20 +36,18 @@ public class Cipher
     // if isEncrypt == false -> encrypted to original
     // works only when the input char is included in our alphabet variables
     // should not replace symbols or upper case letters, return input char in those cases
-    private char replaceChar(char inputChar, boolean isEncrypt) {
+    private char replaceChar(char inputChar, boolean isEncrypt){
         
-        if(isEncrypt) {
+        if (isEncrypt){
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
-                if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                if(ORIGINAL_ALPHABET.charAt(i) == inputChar){
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
-        }
-        else {
-            for (int i = 0; i < CIPHER_ALPHABET.length(); i++)   
-            {
-                if(CIPHER_ALPHABET.charAt(i) == inputChar) {
+        }else{
+            for (int i = 0; i < CIPHER_ALPHABET.length(); i++){
+                if(CIPHER_ALPHABET.charAt(i) == inputChar){
                     return ORIGINAL_ALPHABET.charAt(i);
                 }
             }
@@ -58,4 +56,4 @@ public class Cipher
         // if we did not find it in the alphabet, then return the original char
         return inputChar;
     }
-}   
+}
